c0cab03172510bf215aba28600a83776fdd3e3de
Oukkal Yacine <oukkaly@im2ag-204-09.u-ga.fr>
2023-12-05 15:48:40 +0100

    test de la fonction calcul prix , ajout de commentaires et changement pour la gestion d'erreur

--- calcule_prix.c
+++ calcule_prix.c
@@ -12,25 +12,36 @@ struct trajet{
     char heur_depart[5];
     char heur_arrivee[5];
     float prix;
-    char etat_prix;
-    };
+    char promotion;
+};
 
-//fonction qui calcule les prix et élimine la derniére case
-float calcule_prix(struct trajet trajetClient){
-
-    switch (trajetClient.etat_prix){
-        case 'r':
-            trajetClient.prix = trajetClient.prix - ((20 * trajetClient.prix)/ 100);
+/*
+ * fonction qui calcule les prix suivant la valeur de trajetClient.promotion
+ * si 'r' il y a réduction donc on réduit le prix de 20%
+ * si 's' il y a supplément donc on rajout au prix 10%
+ * sinon on fait rien
+ * 
+ * @param struct trajet
+ * @res int     0 -> erreur 1 -> succes  
+ */
+int calcule_prix(struct trajet *trajetClient){
+    int status = 1 ; // on suppose qu'il y a pas erreur 
+    if(trajetClient->prix > 0){
+        switch (trajetClient->promotion){
+            case 'r':
+                trajetClient->prix = trajetClient->prix - ((20 * trajetClient->prix)/ 100);
+                break;
+            case 's':
+                trajetClient->prix = trajetClient->prix + ((10 * trajetClient->prix)/ 100);
+                break;
+            default:  
             break;
-        case 's':
-            trajetClient.prix = trajetClient.prix + ((10 * trajetClient.prix)/ 100);
-            break;
-        default:  
-
-        break;
+        }
+    }else{
+        printf("le trajet fournie en paramètre à calcule_prix a un prix inférieur à 0 ! \n");
+        status = 0;
     }
-
-    return trajetClient.prix;
+    return status;
 };
 
 int main(int argc, char *argv[]) {
@@ -43,14 +54,16 @@ int main(int argc, char *argv[]) {
    strcpy(trajetAless.ville_arrivee, "Valence");
    strcpy(trajetAless.heur_depart, "12:00");
    strcpy(trajetAless.heur_arrivee, "13:25");
-   trajetAless.prix = 17.60;
-   trajetAless.etat_prix = 'r';
+   trajetAless.prix = 10;
+   trajetAless.promotion = ' ';
 
     //appele de le fonction calcule_prix
-  float nouveau_prix = calcule_prix(trajetAless);
+   if(calcule_prix(&trajetAless) != 0){
+    //affichage du nouveau prix
+    printf("le nouveau prix est : %2f\n", trajetAless.prix);
+   }
+   
 
-   //affichage du nouveau prix
-   printf("le nouveau prix est : %2f\n", nouveau_prix);
    
     return 0;
 
